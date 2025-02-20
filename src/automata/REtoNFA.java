4eb5d0998dca2b6b58d7182e4a1918199d604ee0
AnooshaaAli <anooshasixteen@gmail.com>
2025-02-20 20:23:22 +0500

    regex to nfa complete

--- src/automata/REtoNFA.java
+++ src/automata/REtoNFA.java
@@ -6,45 +6,48 @@ import java.util.stream.Collectors;
 public class REtoNFA {
     private static int stateCounter = 0;
 
-    public static String dissolveRange(String regex, int index) {
+    public static String expandRange(String regex, int index) {
         StringBuilder result = new StringBuilder();
         int i = index;
 
         while (i < regex.length()) {
             char c = regex.charAt(i);
 
-            // Check if we have a character class starting
             if (c == '[') {
                 int closingBracket = regex.indexOf(']', i);
                 if (closingBracket != -1) {
-                    String rangeContent = regex.substring(i + 1, closingBracket); // Extract content inside []
-                    StringBuilder expandedRange = new StringBuilder("("); // Use parentheses for alternation
+                    String rangeContent = regex.substring(i + 1, closingBracket);
+                    StringBuilder expandedRange;
+                    if (regex.charAt(i - 1) != '\\')
+                        expandedRange = new StringBuilder("\\(");
+                    else
+                        expandedRange = new StringBuilder("(");
 
+                    boolean hasRange = false;
                     int j = 0;
                     while (j < rangeContent.length()) {
                         if (j + 2 < rangeContent.length() && rangeContent.charAt(j + 1) == '-') {
-                            // Expand range like a-z
                             char start = rangeContent.charAt(j);
                             char end = rangeContent.charAt(j + 2);
                             for (char ch = start; ch <= end; ch++) {
-                                expandedRange.append(ch).append("|");
+                                expandedRange.append(ch).append("\\|");
                             }
-                            j += 3; // Move past "a-z"
+                            j += 3;
+                            hasRange = true;
                         } else {
-                            // Just append normal characters like '0' in [a-z0-9]
-                            expandedRange.append(rangeContent.charAt(j)).append("|");
+                            expandedRange.append(rangeContent.charAt(j)).append("\\|");
                             j++;
                         }
                     }
 
-                    // Remove last '|' and close parentheses
-                    if (expandedRange.length() > 1) {
+                    if (expandedRange.charAt(expandedRange.length() - 1) == '|') {
                         expandedRange.setLength(expandedRange.length() - 1);
                     }
+
                     expandedRange.append(")");
 
                     result.append(expandedRange);
-                    i = closingBracket + 1; // Move past ']'
+                    i = closingBracket + 1;
                 } else {
                     result.append(c);
                     i++;
@@ -110,87 +113,96 @@ public class REtoNFA {
 
         return new NFA(start, end);
     }
-/*
-    public static NFA regexToNFA(String regex) {
-        Stack<NFA> stack = new Stack<>();
-        Stack<NFA> stack2 = new Stack<>();
 
-        for (char c : regex.toCharArray()) {
-            if (c == '|') {
-                NFA temp = stack.pop();
-                stack2.push(temp);
-            } else {
-                if(!stack.empty()){
-                    NFA first = stack.pop();
-                    NFA second = (createBasicNFA(c));
-                    stack.push(concatenate(first, second));
-                }
-                else{
-                    stack.push(createBasicNFA(c));
-                }
-            }
-        }
+    private static NFA optional(NFA nfa) {
+        State start = new State(stateCounter++);
+        State end = new State(stateCounter++);
 
-        if(!stack.empty()){
-            NFA temp = stack.pop();
-            stack2.push(temp);
-        }
+        start.addTransition('ε', end);
 
-        while(stack2.size() > 1){
-            NFA second = stack2.pop();
-            NFA first = stack2.pop();
-            NFA union = union(first, second);
-            stack2.push(union);
-        }
-        return stack2.pop();
+        start.addTransition('ε', nfa.startState);
+        nfa.endState.addTransition('ε', end);
+
+        return new NFA(start, end);
     }
-*/
 
     public static NFA regexToNFA(String regex) {
         Stack<NFA> tempStack = new Stack<>();
         Stack<NFA>  permStack = new Stack<>();
-        Stack<NFA>  parenStack = new Stack<>();
         Stack<Character> operatorStack = new Stack<>();
 
         for (int i = 0; i < regex.length(); i++) {
             char c = regex.charAt(i);
-            if (c == '[') {
-                regex = dissolveRange(regex, i);
-                System.out.println("Dissolved Regex: " + regex);
-                i = i - 1;
-                continue;
-            } else if (c == '(') {
-                operatorStack.push(c);
-            } else if (c == ')') {
-                permStack.push(tempStack.pop());
-                while (!(operatorStack.peek() == '(')) {
-                    char operator = operatorStack.pop();
-                    if (operator == '|') {
-                        NFA second = permStack.pop();
-                        NFA first = permStack.pop();
-                        permStack.push(union(first, second));
-                    } else if (operator == '*') {
-                        NFA nfa = permStack.pop();
-                        permStack.push(kleeneStar(nfa));
-                    } else if (operator == '+') {
-                        NFA nfa = permStack.pop();
-                        permStack.push(plus(nfa));
+            if (c == '\\') {
+                i++;
+                c = regex.charAt(i);
+                if (c == '[') {
+                    String beforeRange = regex.substring(0, i);
+                    String afterRange = expandRange(regex, i);
+                    regex = beforeRange + afterRange;
+                    System.out.println("Expanded Regex: " + regex);
+                    c = regex.charAt(i);
+                }
+                if (c == '(') {
+                    if (!tempStack.empty()) {
+                        permStack.push(tempStack.pop());
+                    }
+                    operatorStack.push(c);
+                } else if (c == ')') {
+                    permStack.push(tempStack.pop());
+                    while (!(operatorStack.peek() == '(')) {
+                        char operator = operatorStack.pop();
+                        if (operator == '|') {
+                            NFA second = permStack.pop();
+                            NFA first = permStack.pop();
+                            permStack.push(union(first, second));
+                        } else if (operator == '*') {
+                            NFA nfa = permStack.pop();
+                            permStack.push(kleeneStar(nfa));
+                        } else if (operator == '+') {
+                            NFA nfa = permStack.pop();
+                            permStack.push(plus(nfa));
+                        } else if (operator == '?') {
+                            NFA nfa = permStack.pop();
+                            permStack.push(optional(nfa));
+                        } else if (operator == '.') {
+                            operatorStack.push(c);
+                            NFA second = permStack.pop();
+                            NFA first = permStack.pop();
+                            permStack.push(concatenate(first, second));
+                        }
+                    }
+                    operatorStack.pop();
+                    tempStack.push(permStack.pop());
+                } else if (c == '|') {
+                    operatorStack.push(c);
+                    NFA temp = tempStack.pop();
+                    permStack.push(temp);
+                } else if (c == '*') {
+                    NFA temp = tempStack.pop();
+                    tempStack.push(kleeneStar(temp));
+                } else if (c == '+') {
+                    NFA temp = tempStack.pop();
+                    tempStack.push(plus(temp));
+                } else if (c == '?') {
+                    NFA nfa = tempStack.pop();
+                    tempStack.push(optional(nfa));
+                } else if (c == '.') {
+                    operatorStack.push(c);
+                    NFA temp = tempStack.pop();
+                    permStack.push(temp);
+                } else {
+                    i--;
+                    c = regex.charAt(i);
+                    if (!tempStack.empty()) {
+                        NFA first = tempStack.pop();
+                        NFA second = (createBasicNFA(c));
+                        tempStack.push(concatenate(first, second));
+                    } else {
+                        tempStack.push(createBasicNFA(c));
                     }
                 }
-                tempStack.push(permStack.pop());
-            } else if (c == '|') {
-                operatorStack.push(c);
-                NFA temp = tempStack.pop();
-                permStack.push(temp);
-            } else if (c == '*') {
-                operatorStack.push(c);
-                NFA temp = tempStack.pop();
-                permStack.push(temp);
-            } else if (c == '+') {
-                operatorStack.push(c);
-                NFA temp = tempStack.pop();
-                permStack.push(temp);
-            } else {
+        } else {
                 if (!tempStack.empty()) {
                     NFA first = tempStack.pop();
                     NFA second = (createBasicNFA(c));
@@ -201,11 +213,11 @@ public class REtoNFA {
             }
         }
 
-        if (!tempStack.empty()) {
+        while (!tempStack.empty()) {
             permStack.push(tempStack.pop());
         }
 
-        while (!operatorStack.isEmpty() && permStack.size() > 1 && (operatorStack.peek() == '*' || operatorStack.peek() == '+' || operatorStack.peek() == '|')) {
+        while (!operatorStack.isEmpty()) {
             char operator = operatorStack.pop();
             if (operator == '|') {
                 NFA second = permStack.pop();
@@ -217,10 +229,28 @@ public class REtoNFA {
             } else if (operator == '+') {
                 NFA nfa = permStack.pop();
                 permStack.push(plus(nfa));
+            } else if (operator == '?') {
+                NFA nfa = permStack.pop();
+                permStack.push(optional(nfa));
+            } else if (operator == '.') {
+                NFA second = permStack.pop();
+                NFA first = permStack.pop();
+                permStack.push(concatenate(first, second));
             }
         }
 
         return permStack.pop();
+    }
+
+    public static NFA combineAllNFAs(List<NFA> nfas) {
+        if (nfas.isEmpty()) return null;
+
+        NFA combinedNFA = nfas.get(0);
+        for (int i = 1; i < nfas.size(); i++) {
+            combinedNFA = union(combinedNFA, nfas.get(i));
+        }
+
+        return combinedNFA;
     }
 
     private static void printNFATransitionTable(NFA nfa) {
@@ -262,23 +292,43 @@ public class REtoNFA {
         System.out.println("End State: q" + nfa.endState.id);
     }
 
+
     public static void main(String[] args) {
 
         // ----------------------- REGEX --------------------------- //
 
-        String KEYWORDS = "flip|twist|flop|spin|echo|capture|global|return|func|code";
-        String BOOLEAN = "true|false";
-        String DATATYPES = "rizz|alpha|beta|gamma";
-        String IDENTIFIERS = "[a-z]+";
-        String OPERATORS = "+|*|-|/|%|=|<=|>=|==|!=|<|>";
-        String STRING = "\"([^\"]*)\"";
-        String COMMENT = "<<<[\\s\\S]*?>>>|<<.*?>>";
-        String INTEGER = "\\b\\d+\\b";
-        String DECIMAL = "\\b\\d+\\.\\d{1,5}\\b";
-        String CHARACTER = "'[^']'";
-        String PUNCTUATOR = "[{}(),;\\[\\]]";
+        String KEYWORDS = "\\(f\\.l\\.i\\.p\\)\\|\\(t\\.w\\.i\\.s\\.t\\)\\|\\(f\\.l\\.o\\.p\\)\\|\\(s\\.p\\.i\\.n\\)\\|\\(e\\.c\\.h\\.o\\)\\|\\(c\\.a\\.p\\.t\\.u\\.r\\.e\\)\\|\\(g\\.l\\.o\\.b\\.a\\.l\\)\\|\\(r\\.e\\.t\\.u\\.r\\.n\\)\\|\\(f\\.u\\.n\\.c\\)\\|\\(c\\.o\\.d\\.e\\)";
+        String BOOLEAN = "\\(t\\.r\\.u\\.e\\)\\|\\(f\\.a\\.l\\.s\\.e\\)";
+        String DATATYPES = "\\(r\\.i\\.z\\.z\\)\\|\\(a\\.l\\.p\\.h\\.a\\)\\|\\(b\\.e\\.t\\.a\\)\\|\\(g\\.a\\.m\\.m\\.a\\)";
+        String IDENTIFIERS = "\\[a-z]\\+";
+        String OPERATORS = "+\\|*\\|-\\|/\\|%\\|=\\|<=\\|>=\\|==\\|!=\\|<\\|>";
+        String STRING = "\".\\(\\[A-Z]\\|\\[a-z]\\|[0-9]\\| \\|!\\|@\\|#\\|$\\|%\\|^\\|&\\|+\\|*\\|.\\||\\|-\\|_\\|=\\|{\\|}\\|(\\|)\\|[\\|]\\|:\\|;\\|\"\\|,\\|<\\|>\\|/\\|?\\)\\*\\.\"";
+        String COMMENT = "<\\.<\\.<\\.\\(\\[A-Z]\\|\\[a-z]\\|\\[0-9]\\| \\|!\\|\"\\|#\\|$\\|%\\|&\\|'\\|,\\|+\\|*\\|.\\||\\|-\\|/\\|:\\|;\\|<\\|=\\|>\\|?\\|@\\|^\\|_\\|`\\|{\\|}\\|(\\|)\\|[\\|]\\|~\\|'\n'\\)\\*\\.>\\.>\\.>\\|<\\.<\\.\\(\\[A-Z]\\|\\[a-z]\\|\\[0-9]\\| \\|!\\|\"\\|#\\|$\\|%\\|&\\|'\\|,\\|+\\|*\\|.\\||\\|-\\|/\\|:\\|;\\|<\\|=\\|>\\|?\\|@\\|^\\|_\\|`\\|{\\|}\\|(\\|)\\|[\\|]\\|~\\)\\*\\.>\\.>";
+        //String COMM = "<.<.<.([a-b]|[0-1]|{|}|~)*.>.>.>|<.<.([a-b]|[0-1]|{|}|~)*.>.>";
+        String INTEGER = "\\[0-9]\\+";
+        String DECIMAL = "\\[0-9]\\+\\..\\.[0-9]\\?\\.[0-9]\\?\\.[0-9]\\?\\.[0-9]\\?\\.[0-9]\\?";
+        //String OPT = "\\[0-1]\\?";
+        //String OPT2 = "\\[0-1]\\?";
+        String CHARACTER = "'\\.\\(\\[A-Z]\\|[a-z]\\|[0-9]\\| \\|!\\|@\\|#\\|$\\|%\\|^\\|&\\|+\\|*\\|.\\||\\|-\\|_\\|=\\|{\\|}\\|(\\|)\\|[\\|]\\|:\\|;\\|\"\\|,\\|<\\|>\\|/\\|?\\)\\.'";
+        String PUNCTUATOR = "{\\|}\\|,\\|;\\|[\\|]\\|(\\|)";
 
-        NFA identifier_nfa = regexToNFA(COMMENT);
-        printNFATransitionTable(identifier_nfa);
+        List<NFA> nfaList = new ArrayList<>();
+        nfaList.add(regexToNFA(KEYWORDS));
+        nfaList.add(regexToNFA(BOOLEAN));
+        nfaList.add(regexToNFA(DATATYPES));
+        nfaList.add(regexToNFA(IDENTIFIERS));
+        nfaList.add(regexToNFA(OPERATORS));
+        nfaList.add(regexToNFA(STRING));
+        nfaList.add(regexToNFA(COMMENT));
+        nfaList.add(regexToNFA(INTEGER));
+        nfaList.add(regexToNFA(DECIMAL));
+        nfaList.add(regexToNFA(CHARACTER));
+        nfaList.add(regexToNFA(PUNCTUATOR));
+
+        NFA finalNFA = combineAllNFAs(nfaList);
+        printNFATransitionTable(finalNFA);
+
+        System.out.println("Total States: " + finalNFA.countStates());
+
     }
 }
